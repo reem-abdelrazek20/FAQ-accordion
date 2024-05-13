24e584b5b71d0f11c137a9cc2f597991615d1a7e
reem-abdelrazek20 <156788337+reem-abdelrazek20@users.noreply.github.com>
2024-05-13 04:39:35 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,17 @@
+var minus="assets/images/icon-minus.svg";
+var plus ="assets/images/icon-plus.svg";
+var div=document.querySelectorAll(`.q`);
+div.forEach((ele)=>{
+    var Try =ele.querySelector(`p`);
+    var question=ele.querySelector(`.h2`);
+    var button=ele.querySelector(`.click`);
+    button.addEventListener(`click`,()=>{
+        if(Try.style.display==="none") {Try.style.display="block";
+            ;button.src=minus;
+    }else{Try.style.display="none";button.src=plus}
+    });
+    question.addEventListener(`click`,()=>{
+        if(Try.style.display==="none") {Try.style.display="block";
+            ;button.src=minus;
+    }else{Try.style.display="none";button.src=plus}})
+})
